962da335a56c752ce6a7a62a1620801aa4832575
Ryan Carville <ryancarville@gmail.com>
2019-10-15 18:22:39 +0200

    pushign to heroku

--- src/config.js
+++ src/config.js
@@ -2,9 +2,11 @@ module.exports = {
 	API_ENDPOINT: 'http://localhost:8000',
 	PORT: process.env.PORT || 8000,
 	NODE_ENV: process.env.NODE_ENV || 'development',
-	DB_URL:
-		process.env.DB_URL || 'postgresql://ryancarville@localhost/bequia-forum',
+	DATABASE_URL:
+		process.env.DATABASE_URL ||
+		'postgresql://ryancarville@localhost/bequia-forum',
 	CLIENT_ORIGIN: '*',
 	JWT_SECRET: process.env.JWT_SECRET || 'coconutDelight',
-	JWT_EXPIRY: process.env.JWT_EXPIRY || '20m'
+	JWT_EXPIRY: process.env.JWT_EXPIRY || '20m',
+	TEST_DATABASE_URL: process.env.TEST_DATABASE_URL
 };
